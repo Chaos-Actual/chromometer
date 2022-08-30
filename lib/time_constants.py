647de91ee2e4bb9c2b3f91e39dd558d7633db185
McGuire1812 <mcguire.mark.d@gmail.com>
2022-08-30 16:53:17 -0400

    Adding lib folder and my consts file

---
+++ lib/time_constants.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+#Progress Bar Length
+PLEN = 250
+BACKGROUND ='#3299a8'
+#microsecond constants
+MICROSECONDS = 1000000.0
+MICROMINUTES = MICROSECONDS * 60
+MICROHOURS = MICROMINUTES * 60
+MICRODAY  = MICROHOURS * 24 
+#Month calculated in function 
+now = datetime.now()
+if((now.year % 400 == 0) or  
+     (now.year % 100 != 0) and  
+     (now.year % 4 == 0)):   
+     LEAPYEAR = 1
+else:
+    LEAPYEAR =0 
+MICROYEAR = MICRODAY * (365+LEAPYEAR)
+
+def month_days(d,year):
+    match d:
+        case 1:
+            return 31
+        case 2:
+            #check for leap year
+            if (year %400 or (year%100!=0 and year%4==0)):
+                return 29
+            return 28
+        case 3:
+            return 31
+        case 4:
+            return 30
+        case 5:
+            return 31
+        case 6:
+            return 30
+        case 7:
+            return 31
+        case 8:
+            return 31
+        case 9:
+            return 30
+        case 10:
+            return 31
+        case 11:
+            return 30
+        case 12:
+            return 31
+    
--- progress.py
+++ progress.py
@@ -5,34 +5,16 @@ from tkinter.ttk import Progressbar, Style
 from threading import Thread
 from time import sleep
 import time
-
-
+from  lib.time_constants import *
 
 
 #Progress Bar Length
 PLEN = 250
 BACKGROUND ='#3299a8'
-#microsecond constants
-MICROSECONDS = 1000000.0
-MICROMINUTES = MICROSECONDS * 60
-MICROHOURS = MICROMINUTES * 60
-MICRODAY  = MICROHOURS * 24 
-#Month calculated in function 
-now = datetime.now()
-if((now.year % 400 == 0) or  
-     (now.year % 100 != 0) and  
-     (now.year % 4 == 0)):   
-     LEAPYEAR = 1
-else:
-    LEAPYEAR =0 
-MICROYEAR = MICRODAY * (365+LEAPYEAR)
 
 bits =[]
 for i in range(32):
     bits.append("\u25A1")
-
-
-     
             
 app = Tk()
 app_style = Style()
@@ -43,37 +25,6 @@ app.config(background='#3299a8')
 app.title('Progress Bar')
 app.geometry("1000x500")
 canvas = Canvas(app)
-
-
-def month_days(d,year):
-    match d:
-        case 1:
-            return 31
-        case 2:
-            #check for leap year
-            if (year %400 or (year%100!=0 and year%4==0)):
-                return 29
-            return 28
-        case 3:
-            return 31
-        case 4:
-            return 30
-        case 5:
-            return 31
-        case 6:
-            return 30
-        case 7:
-            return 31
-        case 8:
-            return 31
-        case 9:
-            return 30
-        case 10:
-            return 31
-        case 11:
-            return 30
-        case 12:
-            return 31
 
 def  Create_time_labels():
     labels = [
